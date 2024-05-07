4fac4df66b0c8745733f00987fd78c56602873c4
abbassix <mehdi.n.abbassi@gmail.com>
2024-05-07 11:29:48 +0200

    added dir to the config arg

--- finetune.sh
+++ finetune.sh
@@ -2,6 +2,6 @@
 
 # Run Python files with their respective arguments
 # to fine-tune three models
-python finetuning.py compare_reframing
-python finetuning.py compare_masking
-python finetuning.py compare_models
+python finetuning.py finetune_configs/compare_reframing
+python finetuning.py finetune_configs/compare_masking
+python finetuning.py finetune_configs/compare_models
