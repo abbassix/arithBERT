a90d8eb8b446f3e2b023c04c690743ae35237f37
abbassix <mehdi.n.abbassi@gmail.com>
2024-05-07 13:20:48 +0200

    adding baseline fine-tuning

--- finetune.sh
+++ finetune.sh
@@ -2,6 +2,7 @@
 
 # Run Python files with their respective arguments
 # to fine-tune three models
+python finetuning.py finetune_configs/baseline
 python finetuning.py finetune_configs/compare_reframing
 python finetuning.py finetune_configs/compare_masking
 python finetuning.py finetune_configs/compare_models
