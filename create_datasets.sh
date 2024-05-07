02baab4e59f3a576e7a62d4b25243fb67d367f93
abbassix <mehdi.n.abbassi@gmail.com>
2024-05-07 11:28:16 +0200

    added directory name

--- create_datasets.sh
+++ create_datasets.sh
@@ -2,9 +2,9 @@
 
 # Run Python files with their respective arguments
 # to create six datasets
-python create_datasets.py single_digit_train
-python create_datasets.py single_digit_test
-python create_datasets.py double_digit_test
-python create_datasets.py single_digit_original_train
-python create_datasets.py single_digit_original_test
-python create_datasets.py double_digit_original_test
+python create_datasets.py dataset_configs/single_digit_train
+python create_datasets.py dataset_configs/single_digit_test
+python create_datasets.py dataset_configs/double_digit_test
+python create_datasets.py dataset_configs/single_digit_original_train
+python create_datasets.py dataset_configs/single_digit_original_test
+python create_datasets.py dataset_configs/double_digit_original_test
